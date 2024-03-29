6c07d10f173b39db77615faa3edce50a6e72044f
Dvoretskyi Oleksandr <91936573+nypzlst@users.noreply.github.com>
2024-03-29 22:54:42 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = string.Empty;
+                Dictionary<char, int> characterCounts = text.Text
+                    .GroupBy(c => c)
+                    .ToDictionary(grp => grp.Key, grp => grp.Count());
+
+
+                int count = text.Text.Length - text.Text.Distinct().Count();
+                label1.Text += $"Кількість однакових елементів : {count}\n";
+                foreach (var pair in characterCounts)
+                {
+                    label1.Text += $"{pair.Key}: {pair.Value}\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+            
+        }
+    }
+}
